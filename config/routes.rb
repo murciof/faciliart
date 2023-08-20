036a2c65307bcf4552abd09f190898ed95eeb26f
Murcio Filho <murciofilho94@gmail.com>
2023-08-20 14:43:35 -0300

    :sparkles: add Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   root "arts#index"
   resources :arts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
