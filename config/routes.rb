374a5d624d318c37b0b3e963aab6f6ad0544b219
Murcio Filho <murciofilho94@gmail.com>
2023-09-07 14:53:37 -0300

    :recycle: att routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :item_sizes
-  resources :items
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  resources :comments
-  resources :users
   root 'arts#index'
+  resources :comments, except: [:new]
   resources :arts do
     resources :comments, only: [:create]
     resources :orders, only: [:create]
+  end
+  resources :orders, only: [:show]
+  resources :users, only: [:show]
+  scope 'admin' do
+    resources :users, except: [:show]
+    resources :items, except: [:show]
+    resources :item_sizes
+    resources :orders, except: %i[new show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
