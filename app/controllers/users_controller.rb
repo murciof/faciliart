d5da0a4a595bb6b1b18007c3216b4305bc91f2cf
Murcio Filho <murciofilho94@gmail.com>
2023-09-16 20:52:47 -0300

    :art: adjust users index layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
+  layout 'admin', only: [:index]
+
   # GET /users or /users.json
   def index
     @users = User.all
